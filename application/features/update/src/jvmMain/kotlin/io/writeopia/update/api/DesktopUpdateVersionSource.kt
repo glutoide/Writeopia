9d1b99fd7ca09c1a5897a6d190db5f17aed20455
glutoide <glutoide1@gmail.com>
2026-09-17 00:08:20 +0300

    refactor(update): share version response model

--- application/features/update/src/jvmMain/kotlin/io/writeopia/update/api/DesktopUpdateVersionSource.kt
+++ application/features/update/src/jvmMain/kotlin/io/writeopia/update/api/DesktopUpdateVersionSource.kt
@@ -1,6 +1,6 @@
 package io.writeopia.update.api
 
-import io.writeopia.update.model.DesktopAppVersionResponse
+import io.writeopia.sdk.serialization.response.DesktopAppVersionResponse
 
 fun interface DesktopUpdateVersionSource {
     suspend fun latestVersion(): DesktopAppVersionResponse
