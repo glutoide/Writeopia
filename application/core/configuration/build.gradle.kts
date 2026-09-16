2d06190b4f17df97b059dc7d172def1572c53638
glutoide <glutoide1@gmail.com>
2026-09-16 23:55:03 +0300

    build(configuration): generate desktop version from release source

--- application/core/configuration/build.gradle.kts
+++ application/core/configuration/build.gradle.kts
@@ -3,6 +3,33 @@
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
+val desktopAppVersion = rootProject.extra["desktopAppVersion"] as String
+val generatedDesktopVersionDir =
+    layout.buildDirectory.dir("generated/desktopVersion/kotlin")
+
+val generateDesktopAppVersion by tasks.registering {
+    inputs.property("desktopAppVersion", desktopAppVersion)
+    outputs.dir(generatedDesktopVersionDir)
+
+    doLast {
+        val outputFile = generatedDesktopVersionDir
+            .get()
+            .file("io/writeopia/core/configuration/DesktopAppVersion.kt")
+            .asFile
+
+        outputFile.parentFile.mkdirs()
+        outputFile.writeText(
+            """
+            package io.writeopia.core.configuration
+
+            object DesktopAppVersion {
+                const val CURRENT = "$desktopAppVersion"
+            }
+            """.trimIndent()
+        )
+    }
+}
+
 plugins {
     kotlin("multiplatform")
     alias(libs.plugins.androidMultiplatformLibrary)
@@ -44,6 +71,10 @@ kotlin {
     }
 
     sourceSets {
+        val jvmMain by getting {
+            kotlin.srcDir(generatedDesktopVersionDir)
+        }
+
         val commonMain by getting {
             dependencies {
                 implementation(project(":writeopia_models"))
@@ -59,4 +90,9 @@ kotlin {
             }
         }
     }
+}
+
+
+tasks.named("compileKotlinJvm") {
+    dependsOn(generateDesktopAppVersion)
 }
