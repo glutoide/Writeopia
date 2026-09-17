4ef4306cbb1d7707854fbc27638730ae22cacc44
glutoide <glutoide1@gmail.com>
2026-09-17 09:27:41 +0300

    test: verify auth app version endpoint

--- application/features/update/src/jvmTest/kotlin/io/writeopia/update/api/DesktopUpdateApiTest.kt
+++ application/features/update/src/jvmTest/kotlin/io/writeopia/update/api/DesktopUpdateApiTest.kt
@@ -40,7 +40,7 @@ class DesktopUpdateApiTest {
 
         assertEquals("0.48.0", response.version)
         assertEquals(
-            "https://writeopia.io/api/app/desktop-version",
+            "https://writeopia.io/auth/app/version",
             requestedUrl
         )
     }
